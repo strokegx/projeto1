7c67ac388f8b96c0bb88b16575fb19339323159d
Nicolas <Na070162@gmail.com>
2026-03-16 12:10:25 -0300

    corrigindo app.py para deploy

--- projeto1/app.py
+++ projeto1/app.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template, request
 import random
+import os
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     numero = None
-
     if request.method == "POST":
-        print("gerando o seu número lendário...")
         numero = random.randint(1, 100)
-
     return render_template("index.html", numero=numero)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
