5660dc53e1c170d5b713e06e8bc206616566cb05
Nicolas <Na070162@gmail.com>
2026-03-16 10:32:47 -0300

    atualizando site do número lendário

---
+++ projeto1/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+import random
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    numero = None
+
+    if request.method == "POST":
+        print("gerando o seu número lendário...")
+        numero = random.randint(1, 100)
+
+    return render_template("index.html", numero=numero)
+
+if __name__ == "__main__":
+    app.run(debug=True)
